7c98aff9166fb77d8d7365f882ab3bd267c73c4f
Jeferson-Js <oliveira.jeferson.jso@gmail.com>
2023-08-10 15:17:01 -0300

    Add the car images and cards forEach card. footer and header finished.

--- lock/views.py
+++ lock/views.py
@@ -6,7 +6,116 @@ def index(request):
 
 def novidades(request):
     card_range = range(20)
-    return render(request, 'novidades.html', {'card_range': card_range})
+    carros_localiza = [
+        {
+            "nome": "Sedan Compacto",
+            "cor": "Prata",
+            "imagem": "img/ARGO.png"
+        },
+        {
+            "nome": "SUV Médio",
+            "cor": "Preto",
+            "imagem": "img/carros.png"
+        },
+        {
+            "nome": "Hatchback Esportivo",
+            "cor": "Vermelho",
+            "imagem": "img/carros.png"
+        },
+        {
+            "nome": "Minivan Familiar",
+            "cor": "Branco",
+            "imagem": "img/carros.png"
+        },
+        {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+         {
+            "nome": "Caminhonete",
+            "cor": "Azul",
+            "imagem": "img/carros.png"
+        },
+        # ... adicione informações de cor e imagem para os outros carros ...
+    ]
+
+    return render(request, 'novidades.html', {'card_range': card_range, 'carros_localiza': carros_localiza})
 
 def reservas(request):
     return render(request, 'reservas.html')
@@ -18,7 +127,9 @@ def promocoes(request):
     return render(request, 'promocoes.html')
 
 def detalhes(request, id):
-    return render(request, 'detalhes.html', {'id': id})
+    card_number = int(id) + 1
+    return render(request, 'detalhes.html', {'id': card_number})
+
 
 def pagina_404(request, exception):
     return render(request, '404.html', status=404)
